9d0abaafd5ac473af41f9444e9007c82b1fc122e
loftyyyy <lofty08.business@gmail.com>
2025-09-10 02:01:04 +0800

    Initial Commit

---
+++ src/test/java/com/rho/srpingsecurity/SrpingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rho.srpingsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
