7b6b66eba2d723d843feb5e8e96d9eac43c26150
loftyyyy <lofty08.business@gmail.com>
2025-09-10 12:30:28 +0800

    Getting Started with Security

---
+++ src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
@@ -0,0 +1,12 @@
+package com.rho.srpingsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+//    SecurityFilterChain configure(HttpSecurity http) throws Exception{
+//
+//    }
+}
