bb4d4101374e98f1c2d8fa7f66dd8061bc132ce0
loftyyyy <lofty08.business@gmail.com>
2025-09-11 21:22:31 +0800

    Bean is needed in a configuration class to be called on its method

--- src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
+++ src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.rho.srpingsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+
+    @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception{
         http.httpBasic(Customizer.withDefaults());
 
