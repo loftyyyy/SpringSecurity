9c51a48cbb226a0da6d758d09e805db6c01ad311
loftyyyy <lofty08.business@gmail.com>
2025-09-11 21:07:51 +0800

    Implemented a HttpSecurity Object

--- src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
+++ src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
@@ -1,12 +1,17 @@
 package com.rho.srpingsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
-//    SecurityFilterChain configure(HttpSecurity http) throws Exception{
-//
-//    }
+    SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(C -> C.anyRequest().permitAll());
+
+        return http.build();
+    }
 }
