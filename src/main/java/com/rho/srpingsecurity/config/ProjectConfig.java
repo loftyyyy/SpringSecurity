ae84c4a84de49eb44b5aa7a0bddaeb23e749dda2
loftyyyy <lofty08.business@gmail.com>
2025-10-19 20:05:57 +0800

    InMemoryUserDetailsManager()

--- src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
+++ src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
@@ -4,18 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
+    @Bean
+    UserDetailsService userDetailsService(){
+        var user = User.withUsername("user")
+                .password("1234")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 //    @Bean
-//    SecurityFilterChain configure(HttpSecurity http) throws Exception{
+//    PasswordEncoder passwordEncoder(){
 //
-//        http.httpBasic(Customizer.withDefaults());
-//
-//        http.authorizeHttpRequests(C -> C.anyRequest().permitAll());
-//
-//        return http.build();
 //    }
 }
