884d8127a26e4836ad613fb0fa2c7679f8799dd5
loftyyyy <lofty08.business@gmail.com>
2025-10-19 22:27:20 +0800

    NoOpPasswordEncoder().instance()

--- src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
+++ src/main/java/com/rho/srpingsecurity/config/ProjectConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,8 +23,8 @@ public class ProjectConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
-//    @Bean
-//    PasswordEncoder passwordEncoder(){
-//
-//    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
