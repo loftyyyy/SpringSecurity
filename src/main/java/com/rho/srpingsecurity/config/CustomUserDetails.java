57146ab625e0d13b3ebbeb1bce54dcbd0414461c
loftyyyy <lofty08.business@gmail.com>
2025-10-21 23:34:04 +0800

    Implemented In Memory User Details Service, the thing which User Details

--- src/main/java/com/rho/srpingsecurity/config/CustomUserDetails.java
+++ src/main/java/com/rho/srpingsecurity/config/CustomUserDetails.java
@@ -9,15 +9,25 @@ import java.util.Collection;
 import java.util.List;
 
 public class CustomUserDetails implements UserDetails {
-    private final User user;
+//    private final User user;
+//
+//    public CustomUserDetails(User user){
+//        this.user = user;
+//    }
 
-    public CustomUserDetails(User user){
-        this.user = user;
+    private final String username;
+    private final String password;
+    private final String authority;
+
+    public CustomUserDetails(String username, String password, String authority){
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "READ");
+        return List.of(() -> authority);
     }
 
     @Override
